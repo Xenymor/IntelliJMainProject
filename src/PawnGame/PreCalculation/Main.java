57567eaf1ffd4f36676225cd2c4b596a7ea5b4a0
magictimon <retzlaff.timon@gmail.com>
2021-08-13 21:31:56 +0200

    changed everything to 4x4 and int next step to make half bits work

--- src/PawnGame/PreCalculation/Main.java
+++ src/PawnGame/PreCalculation/Main.java
@@ -3,9 +3,7 @@ package PawnGame.PreCalculation;
 import PawnGame.Move;
 import PawnGame.PlayerType;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -15,13 +13,20 @@ import static PawnGame.PlayerType.*;
 public class Main {
     private final int fieldSize = 4;
     HashMap<Integer, List<Integer>> knownPositions = new HashMap<>();
-    String pathname = "positions1.txt";
+    private HashMap<Integer, Integer> scores;
 
-    public static void main(String[] args) throws IllegalMoveException, InterruptedException, IOException, ClassNotFoundException, WTFIsWrongWithYouException {
-        new Main().test();
+    public static void main(String[] args) throws IllegalMoveException, InterruptedException, IOException, ClassNotFoundException, WhyAreYouEditingMyCodeException {
+        new Main().run();
     }
 
-    private void test() throws IOException, ClassNotFoundException {
+    private void printStartingScore() throws IOException, ClassNotFoundException, WhyAreYouEditingMyCodeException {
+        ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream("scores.txt"));
+        HashMap<Integer, Integer> scores = (HashMap<Integer, Integer>) objectInputStream.readObject();
+        int startingPositionAsInt = getStartingPositionAsInt();
+        System.out.println(startingPositionAsInt + " = " + scores.get(startingPositionAsInt));
+    }
+
+    private int getStartingPositionAsInt() throws WhyAreYouEditingMyCodeException {
         List<Figure> figures = new ArrayList<>();
         for (int x = 0; x < fieldSize; x++) {
             for (int y = 0; y < fieldSize; y++) {
@@ -36,11 +41,10 @@ public class Main {
                 }
             }
         }
-        int field = posToInt(figures, PLAYER1);
-        System.out.println(field);
+        return posToInt(figures, PLAYER1);
     }
 
-    private void run() throws IllegalMoveException, WTFIsWrongWithYouException {
+    private void run() throws IllegalMoveException, WhyAreYouEditingMyCodeException, IOException {
         List<Figure> figures = new ArrayList<>();
         for (int x = 0; x < fieldSize; x++) {
             for (int y = 0; y < fieldSize; y++) {
@@ -58,26 +62,61 @@ public class Main {
         int field = posToInt(figures, PLAYER1);
         knownPositions.put(field, new ArrayList<>());
         addAllMoves(field);
-        HashMap<Integer, Float> scores = new HashMap<>();
-        for (int position = 0; position < Integer.MAX_VALUE; position++) {
-            if (knownPositions.containsKey(position)) {
-                PlayerType wonPlayerType = whoHasWon(intToPosition(position));
-                if (wonPlayerType != NEUTRAL) {
-                    if (wonPlayerType == PLAYER1) {
-
-                    } else if (wonPlayerType == PLAYER2) {
-
-                    } else {
-                        throw new WTFIsWrongWithYouException();
-                    }
+        scores = new HashMap<>();
+        Integer[] keys = knownPositions.keySet().toArray(new Integer[1]);
+        for (Integer aInteger : keys) {
+            PlayerType wonPlayerType = whoHasWon(intToPos(aInteger, fieldSize));
+            if (wonPlayerType != NEUTRAL) {
+                if (wonPlayerType == PLAYER1) {
+                    setScores(aInteger, 1);
+                } else if (wonPlayerType == PLAYER2) {
+                    setScores(aInteger, -1);
+                } else {
+                    throw new WhyAreYouEditingMyCodeException();
                 }
             }
         }
+        for (Integer key : keys) {
+            if (!scores.containsKey(key)) {
+                scores.put(key, 0);
+            }
+        }
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(new FileOutputStream("scores.txt"));
+        objectOutputStream.writeObject(scores);
+        objectOutputStream.close();
+        System.out.println("Finished");
+        System.out.println(scores);
     }
 
+    private void setScores(Integer position, int scoreToSet) throws WhyAreYouEditingMyCodeException {
+        if (scores.containsKey(position)) {
+            Position pos = intToPos(position, fieldSize);
+            if (pos.getPlayerToMove() == PLAYER1) {
+                if (scoreToSet < scores.get(position)) {
+                    scores.put(position, scoreToSet);
+                }
+            } else if (pos.getPlayerToMove() == PLAYER2) {
+                if (scoreToSet > scores.get(position)) {
+                    scores.put(position, scoreToSet);
+                }
+            } else {
+                throw new WhyAreYouEditingMyCodeException();
+            }
+        } else {
+            scores.put(position, scoreToSet);
+        }
+        List<Integer> intList = knownPositions.get(position);
+        if (intList == null)
+            return;
+        List<Integer> prev = intList.subList(0, intList.size());
+        knownPositions.remove(position);
+        for (Integer aInteger : prev) {
+            setScores(aInteger, -scoreToSet);
+        }
+    }
 
-    private void addAllMoves(int field) throws IllegalMoveException {
-        Position pos = intToPosition(field);
+    private void addAllMoves(int field) throws IllegalMoveException, WhyAreYouEditingMyCodeException {
+        Position pos = intToPos(field, fieldSize);
         PlayerType playerToMove = pos.getPlayerToMove();
         PlayerType wonPlayer = whoHasWon(pos);
         if (wonPlayer != NEUTRAL) {
@@ -184,7 +223,7 @@ public class Main {
         return true;
     }
 
-    private int posToInt(List<Figure> figures, PlayerType playerToMove) {
+    public static int posToInt(List<Figure> figures, PlayerType playerToMove) throws WhyAreYouEditingMyCodeException {
         int result = 0;
         result |= playerToMove == PLAYER1 ? 0 : playerToMove == PLAYER2 ? 1 : -1;
         for (Figure figure : figures) {
@@ -195,12 +234,14 @@ public class Main {
             } else if (type == PLAYER2) {
                 result |= 1 << (figure.getX() * 2 + figure.getY() * 8) + 1;
                 result |= 0 << (figure.getX() * 2 + figure.getY() * 8) + 2;
+            } else {
+                throw new WhyAreYouEditingMyCodeException();
             }
         }
         return result;
     }
 
-    private Position intToPosition(int number) {
+    public static Position intToPos(Integer number, int fieldSize) {
         List<Figure> figures = new ArrayList<>();
         int i = (number) & 1;
         PlayerType playerToMove = i == 0 ? PLAYER1 : PLAYER2;
@@ -221,6 +262,6 @@ public class Main {
     private static class IllegalMoveException extends Throwable {
     }
 
-    private static class WTFIsWrongWithYouException extends Throwable {
+    private static class WhyAreYouEditingMyCodeException extends Throwable {
     }
 }
